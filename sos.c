776d6b98000a15ee338a222dcd67cc3858fb4b69
Cecilia Betancourt <cebetanc@usc.edu>
2026-03-25 19:45:30 -0700

    Update sos.c

--- sos.c
+++ sos.c
@@ -1,2 +1,7 @@
 // file for formatting message and communicating with antenna
 
+// sos initilization (checking gps and modem)
+
+// sos formatting of message
+
+// sos attempting to send (success or failure? how many attempts?)
