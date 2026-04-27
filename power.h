30e7aba4985f0375335c073612e8193b8786c237
Cecilia Betancourt <cebetanc@usc.edu>
2026-04-27 12:10:05 -0700

    Add files via upload

---
+++ power.h
@@ -0,0 +1,6 @@
+#ifndef POWER_H
+#define POWER_H
+#include <stdint.h>
+void    power_init(void);
+uint8_t power_is_low(void);
+#endif
---
+++ waypoint.h
@@ -0,0 +1,5 @@
+#ifndef WAYPOINT_H
+#define WAYPOINT_H
+void waypoint_init(void);
+void waypoint_save(void);
+#endif
